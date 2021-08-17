c8fa4afc35c6c39f4e1c02d0f429918693eaa7a5
Elkinssm <elkinsilvamanrique@gmail.com>
2021-08-16 21:30:29 -0500

    primer ejercicio js

---
+++ practica.js
@@ -0,0 +1,71 @@
+// Cepillarse los dientes
+
+// Comprobamos si tenemos cepillo de dientes
+let tienesCepillo = true;
+
+// Comprobar si tenemos pasta de dientes
+let tienesPasta = true;
+
+// Comiste algo
+let comida = true;
+
+// Horario donde te cepillas siempre
+const horaFija = "08:30 p.m";
+
+//Hora actual
+let horaActual = "09:14 p.m";
+
+// Tiempo que tardas en cepillarte
+let tiempoCepillarte = "2 minutos";
+
+// Numero de cepilladas
+let numeroCepilladas = 45;
+
+//Array
+let cepillarse = [
+  tienesCepillo,
+  tienesPasta,
+  comida,
+  horaFija,
+  horaActual,
+  tiempoCepillarte,
+  numeroCepilladas,
+];
+console.log(cepillarse);
+
+//Segundo ejercicio practico
+
+//Irse dormir
+
+//Te cepillaste los dientes
+let cepillarte = true;
+
+//Cenaste
+let cenaste = false;
+
+//Progamas el despertador
+const despertar = "08:15 a.m";
+
+//Cuanto luces apagaste
+let numeroLuces = 3;
+
+//Cantidad de frasadas
+let cantidadFrasadas = 4;
+
+//Cantidad de almohadas
+let cantidadAlmohadas = 2;
+
+//Numero de horas que dormiste
+let horasDormidas = "8 horas";
+
+let dormir = [
+  cepillarte,
+  cenaste,
+  despertar,
+  numeroLuces,
+  cantidadFrasadas,
+  cantidadFrasadas,
+  horasDormidas,
+];
+
+console.log(dormir);
